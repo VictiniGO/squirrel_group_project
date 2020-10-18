05edd623e172c614830fdfe0b0b2081fed738328
Yanjun Wan <yw3580@columbia.edu>
2020-10-18 20:42:02 +0000

    complete import command and model:SquirrelDetails

--- squirrelmap/squirrelapp/management/commands/import_squirrel_data.py
+++ squirrelmap/squirrelapp/management/commands/import_squirrel_data.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-from squirrelapp.models import SquirrelDetail
+from squirrelapp.models import SquirrelDetails
 import csv
+import json
 
 class Command(BaseCommand):
 
@@ -15,8 +16,38 @@ class Command(BaseCommand):
         with open(file_) as fp:
             reader=csv.DictReader(fp)
             for line in reader:
-                obj = SquirrelDetail()
-                obj.X = line['X']
+                obj = SquirrelDetails()
+                obj.Latitude = line['X']
+                obj.Logitude = line['Y']
+                obj.Unique_Squirrel_ID= line['Unique Squirrel ID']
+                obj.Hectare=line['Hectare']
+                obj.Shift=line['Shift']
+                obj.Date=line['Date']
+                obj.Hectare_Squirrel_Number=line['Hectare Squirrel Number']
+                obj.Age=line['Age']
+                obj.Primary_Fur_Color=line['Primary Fur Color']
+                obj.Highlight_Fur_Color=line['Highlight Fur Color']
+                obj.Combination_of_Primary_and_Highlight_Color=line['Combination of Primary and Highlight Color']
+                obj.Color_notes=line['Color notes']
+                obj.Location=line['Location']
+                obj.Above_Ground_Sighter_Measurement=line['Above Ground Sighter Measurement']
+                obj.Specific_Location=line['Specific Location']
+                obj.Running= json.loads(line['Running'].lower())
+                obj.Chasing=json.loads(line['Chasing'].lower())
+                obj.Climbing=json.loads(line['Climbing'].lower())
+                obj.Eating=json.loads(line['Eating'].lower())
+                obj.Foraging=json.loads(line['Foraging'].lower())
+                obj.Other_Activities=line['Other Activities']
+                obj.Kuks=json.loads(line['Kuks'].lower())
+                obj.Quaas=json.loads(line['Quaas'].lower())
+                obj.Moans=json.loads(line['Moans'].lower())
+                obj.Tail_flags=json.loads(line['Tail flags'].lower())
+                obj.Tail_twitches=json.loads(line['Tail twitches'].lower())
+                obj.Approaches=json.loads(line['Approaches'].lower())
+                obj.Indifferent=json.loads(line['Indifferent'].lower())
+                obj.Runs_from=json.loads(line['Runs from'].lower())
+                obj.Other_Interactions=line['Other Interactions']
+                obj.Lat_Long=line['Lat/Long']
                 obj.save()
         
         msg=f'You are importing from {file_}'
--- squirrelmap/squirrelapp/models.py
+++ squirrelmap/squirrelapp/models.py
@@ -1,3 +1,153 @@
 from django.db import models
 
-# Create your models here.
+class SquirrelDetails(models.Model):
+    Latitude = models.DecimalField(
+            max_digits=15, 
+            decimal_places=10,
+            null=True)
+
+    Longitude = models.DecimalField(
+            max_digits=15, 
+            decimal_places=10,
+            null=True)
+
+    Unique_Squirrel_ID = models.CharField(
+        max_length=20,
+        primary_key=True,
+    )
+
+    Hectare = models.CharField(max_length=3)
+    
+    SHIFT_CHOICES = [
+    ('AM','AM'),
+    ('PM','PM'),
+]
+
+    Shift = models.CharField(
+        max_length=2,
+        choices=SHIFT_CHOICES,
+    )
+
+    Date = models.CharField(max_length=8)
+
+    Hectare_Squirrel_Number = models.IntegerField
+
+    AGE_CHOICES = [
+    ('Adult','Adult'),
+    ('Juvenile','Juvenile'),
+]
+    
+    Age = models.CharField(
+        max_length=10,
+        choices=AGE_CHOICES,
+        null=True,
+    )
+
+    PRIMARY_FUR_COLOR_CHOICES = [
+    ('Gray','Gray'),
+    ('Cinnamon','Cinnamon'),
+    ('Black','Black'),
+]
+        
+    Primary_Fur_Color = models.CharField(
+        max_length=10,
+        choices=PRIMARY_FUR_COLOR_CHOICES,
+        null=True,
+    )
+
+        
+    Highlight_Fur_Color = models.CharField(
+        max_length=50
+    )
+    
+
+    Combination_of_Primary_and_Highlight_Color = models.CharField(
+        max_length=50
+    )
+
+    Color_notes = models.CharField(
+        max_length=50
+    )
+
+    LOCATION_CHOICES = [
+    ('Ground Plane','Ground Plane'),
+    ('Above Ground','Above Ground'),
+]
+    
+    Location = models.CharField(
+        max_length=20,
+        choices=LOCATION_CHOICES,
+        null=True,
+    )
+
+    Above_Ground_Sighter_Measurement= models.CharField(
+        max_length=10
+    )
+
+    Specific_Location = models.CharField(
+        max_length=50
+    )
+
+    Running = models.BooleanField(
+        null=False,
+    )
+
+    Chasing = models.BooleanField(
+        null=False,
+    )
+
+    Climbing = models.BooleanField(
+        null=False,
+    )
+    
+    Eating = models.BooleanField(
+        null=False,
+    )   
+    
+    Foraging = models.BooleanField(
+        null=False,
+    )
+    
+    Other_Activities = models.CharField(
+        max_length=50
+    )
+    
+    Kuks = models.BooleanField(
+        null=False,
+    )  
+    
+    Quaas  = models.BooleanField(
+        null=False,
+    )  
+    
+    Moans = models.BooleanField(
+        null=False,
+    )      
+    
+    Tail_flags = models.BooleanField(
+        null=False,
+    ) 
+    
+    Tail_twitches = models.BooleanField(
+        null=False,
+    )       
+    
+    Approaches  = models.BooleanField(
+        null=False,
+    )
+    
+    Indifferent = models.BooleanField(
+        null=False,
+    )
+    
+    Runs_from  = models.BooleanField(
+        null=False,
+    ) 
+    
+    Other_Interactions = models.CharField(
+        max_length=50
+    )  
+    
+    Lat_Long = models.CharField(
+        max_length=50
+    )
