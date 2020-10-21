540c8ad9f637323b631b02100d7940c40843ae94
Yanjun Wan <yw3580@columbia.edu>
2020-10-21 23:18:41 +0000

    sightings + import/export

--- squirrelmap/squirrelapp/management/commands/export_squirrel_data.py
+++ squirrelmap/squirrelapp/management/commands/export_squirrel_data.py
@@ -2,27 +2,19 @@ from django.core.management.base import BaseCommand, CommandError
 from squirrelapp.models import SquirrelDetails
 import csv
 
-
 class Command(BaseCommand):
     help = 'Export squirrel file'
     def add_arguments(self, parser):
         parser.add_argument('squirrel_file_path', help="file path to be exported")
-
     def handle(self, *args, **options):
         path_=options['squirrel_file_path']
         with open(path_, 'w') as f:
-            fieldnames = ['Latitude', 'Longitude', 'Unique_Squirrel_ID', 'Hectare', 'Shift', 'Date', 'Hectare_Squir
-rel_Number', 'Age', 'Primary_Fur_Color', 'Highlight_Fur_Color', 'Combination_of_Primary_and_Highlight_Color', 'Colo
-r_notes', 'Location', 'Above_Ground_Sighter_Measurement', 'Specific_Location', 'Running', 'Chasing', 'Climbing', 'E
-ating', 'Foraging', 'Other_Activities', 'Kuks', 'Quaas', 'Moans', 'Tail_flags', 'Tail_twitches', 'Approaches', 'Ind
-ifferent', 'Runs_from', 'Other_Interactions', 'Lat_Long']
+            fieldnames = ['Latitude', 'Longitude', 'Unique_Squirrel_ID', 'Hectare', 'Shift', 'Date', 'Hectare_Squirrel_Number', 'Age', 'Primary_Fur_Color', 'Highlight_Fur_Color', 'Combination_of_Primary_and_Highlight_Color', 'Color_notes', 'Location', 'Above_Ground_Sighter_Measurement', 'Specific_Location', 'Running', 'Chasing', 'Climbing','Eating', 'Foraging', 'Other_Activities', 'Kuks', 'Quaas', 'Moans', 'Tail_flags', 'Tail_twitches', 'Approaches','Indifferent', 'Runs_from', 'Other_Interactions', 'Lat_Long']
             writer = csv.writer(f)
             writer.writerow(fieldnames)
             for obj in SquirrelDetails.objects.all():
                 row = [getattr(obj, field) for field in fieldnames]
                 writer.writerow(row)
             f.close()
-
         msg=f'Data written to {path_}'
         self.stdout.write(self.style.SUCCESS(msg))
-
