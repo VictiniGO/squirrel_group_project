83c455d24b51e644c6bd08879b44c0e603879c94
Yanjun Wan <yw3580@columbia.edu>
2020-10-22 01:05:54 +0000

    Edit paths

--- squirrelmap/squirrelapp/viewsurls.py
+++ squirrelmap/squirrelapp/viewsurls.py
@@ -6,9 +6,7 @@ app_name = 'squirrelapp'
 
 urlpatterns = [
     path('',views.index, name = 'index'),
-    path('<slug:squirrel_id>/', views.update, name='update'),
     path('add/',views.sightingsadd),
     path('stats/',views.sightingsstats),
-    # Need to find out how to direct to path /sightings/<unique-squirrel-id>
-    # path('',views.sightingslist),
+    path('<slug:squirrel_id>/', views.update, name='update'),
     ]
