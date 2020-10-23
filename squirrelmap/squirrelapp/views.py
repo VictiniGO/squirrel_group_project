ce40acddd559d9f0c58fabe80a2b8922747679a5
Yanjun Wan <yw3580@columbia.edu>
2020-10-23 00:25:28 +0000

    Add stats view

--- squirrelmap/squirrelapp/views.py
+++ squirrelmap/squirrelapp/views.py
@@ -1,10 +1,11 @@
+# vim squirrelapp/views.py
 from django.urls import reverse
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Avg, Max, FloatField, Count, Q
 
 from .forms import SquirrelRequestForm
 from .models import SquirrelDetails
-
 
 def map(request):
     sightings = SquirrelDetails.objects.all()[:100]
@@ -19,7 +20,6 @@ def index(request):
             'sightings': sightings,
     }
     return render(request,'squirrelapp/index.html',context)
-
 def update(request, squirrel_id):
     sighting = get_object_or_404(SquirrelDetails, pk=squirrel_id)
     context = {
@@ -30,14 +30,12 @@ def update(request, squirrel_id):
 def sightingsadd(request):
     if request.method == 'POST':
         form = SquirrelRequestForm(request.POST)
-
         if form.is_valid():
             form.Latitude = form.cleaned_data['Latitude']
             form.Longitude = form.cleaned_data['Longitude']
             form.Unique_Squirrel_ID = form.cleaned_data['Unique_Squirrel_ID']
             form.Shift = form.cleaned_data['Shift']
             form.Age = form.cleaned_data['Age']
-
             form.save()
         else:
             return JsonResponse({'errors': form.errors}, status=400)
@@ -47,5 +45,22 @@ def sightingsadd(request):
 
 def sightingsstats(request):
     counts = SquirrelDetails.objects.count()
-    return render(request,'squirrelapp/sightingsstats.html',{})
-   
+    am_counts = SquirrelDetails.objects.filter(Shift__exact='AM').count()
+    pm_counts = SquirrelDetails.objects.filter(Shift__exact='PM').count()
+    adult_counts = SquirrelDetails.objects.filter(Age__exact='Adult').count()
+    juvenile_counts = SquirrelDetails.objects.filter(Age__exact='Juvenile').count()
+    gray_counts = SquirrelDetails.objects.filter(Primary_Fur_Color__exact='Gray').count()
+    cinnamon_counts = SquirrelDetails.objects.filter(Primary_Fur_Color__exact='Cinnamon').count()
+    running_counts = SquirrelDetails.objects.filter(Running__exact='True').count()
+    
+    context={
+        'counts':counts,
+        'am_counts':am_counts,
+        'pm_counts':pm_counts,
+        'adult_counts':adult_counts,
+        'juvenile_counts':juvenile_counts,
+        'gray_counts':gray_counts,
+        'cinnamon_counts':cinnamon_counts,
+        'running_counts':running_counts,
+    }
+    return render(request,'squirrelapp/sightingsstats.html',context)
